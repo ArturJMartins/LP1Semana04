c36e849677152763ea3cf212c895679e43f48b37
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-14 10:50:11 +0000

    Método com diferentes parâmetros

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        private static string Decor(string s, char dec, int num)
+        {
+            string agrupar = new string(dec,num);
+            return $"{agrupar} {s} {agrupar}";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0],char.Parse(args[1]),
+            int.Parse(args[2])));
+        }
+    }
+}
