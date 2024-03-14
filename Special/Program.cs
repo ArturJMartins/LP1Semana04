c5969a19c493511bd9f4fc8c638ad52286a3d4be
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-14 11:50:51 +0000

    Testar vários inteiros

---
+++ Special/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            int numero;
+            if(n == 0)
+            {
+                numero = 0;
+            }
+            else if(n == 1)
+            {
+                numero = 1;
+            }
+            else
+            {
+                numero = GetSpecial(n-1) + (2* GetSpecial(n-2));
+            }
+
+            return numero;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSpecial(0));
+            Console.WriteLine(GetSpecial(1));
+            Console.WriteLine(GetSpecial(2));
+            Console.WriteLine(GetSpecial(7));
+        }
+    }
+}
